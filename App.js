08f3f09177675b9b8aa9949572e0c2a22e7d1347
malaFillatunnida <fillatunnida@gmail.com>
2023-09-04 23:08:53 +0700

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+// import { Provider } from "react-redux";
+// import store from "./store";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Login from "./screens/Login";
+// import Home from "./screens/Home";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <Provider store={store}>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+
+        {/* <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        /> */}
+
+      </Stack.Navigator>
+    </NavigationContainer>
+    // </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    flexDirection: "center",
+    justifyContent: "center",
   },
 });
